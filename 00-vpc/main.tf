503c796eb4ece84f45a744b784937559c02b5b53
soumya <korlagani.soumya@gmail.com>
2026-03-21 15:53:57 +0530

    terraform

---
+++ 00-vpc/main.tf
@@ -0,0 +1,6 @@
+module "vpc" {
+    source = "git::https://github.com/soumyakorlaganti/terraformm-aws-vpc.git?ref=main"
+    project = var.project
+    environment = var.environment
+    is_peering_required = true
+}
---
+++ 10-sg/parameters.tf
@@ -0,0 +1,5 @@
+resource "aws_ssm_parameter" "mongodb_sg_id" {
+  name  = "/${var.project}/${var.environment}/mongodb_sg_id"
+  type  = "String"
+  value = module.sg.sg_id
+}
