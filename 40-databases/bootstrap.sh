a590e4e10e85f958ab2cae9fd41ad13a207b2c7a
soumya <korlagani.soumya@gmail.com>
2026-03-22 19:41:57 +0530

    terraform

--- 40-databases/bootstrap.sh
+++ 40-databases/bootstrap.sh
@@ -7,4 +7,5 @@ cd /home/ec2-user
 git clone https://github.com/soumyakorlaganti/ansible-roboshop-roles-tf.git
 
 cd ansible-roboshop-roles-tf
+git pull
 ansible-playbook -e component=$component roboshop.yaml
