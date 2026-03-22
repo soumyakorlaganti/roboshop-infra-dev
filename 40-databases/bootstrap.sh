db7d40c8c749cf2385ce2a1c286cf0c901cfab39
soumya <korlagani.soumya@gmail.com>
2026-03-22 17:15:13 +0530

    terraform

--- 40-databases/bootstrap.sh
+++ 40-databases/bootstrap.sh
@@ -1,1 +1,10 @@
+#!/bin/bash
+
+component=$1
 dnf install ansible -y
+
+cd /home/ec2-user
+git clone https://github.com/soumyakorlaganti/ansible-roboshop-roles-tf.git
+
+cd ansible-roboshop-roles-tf
+ansible-playbook -e component=$component roboshop.yaml
