c719dee981b059771ea8693c8a26a0fb22a746ad
soumya <korlagani.soumya@gmail.com>
2026-03-22 19:38:51 +0530

    terraform

--- 20-sg-rules/data.tf
+++ 20-sg-rules/data.tf
@@ -18,6 +18,10 @@ data "aws_ssm_parameter" "redis_sg_id" {
   name = "/${var.project}/${var.environment}/redis_sg_id"
 }
 
+data "aws_ssm_parameter" "mysql_sg_id" {
+  name = "/${var.project}/${var.environment}/mysql_sg_id"
+}
+
 data "aws_ssm_parameter" "catalogue_sg_id" {
   name = "/${var.project}/${var.environment}/catalogue_sg_id"
 }
