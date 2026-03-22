5d019cd07fd1745e522917e2ffae9745000e448f
soumya <korlagani.soumya@gmail.com>
2026-03-22 17:53:41 +0530

    terraform

--- 40-databases/main.tf
+++ 40-databases/main.tf
@@ -37,3 +37,43 @@ resource "terraform_data" "bootstrap" {
     ]
   }
 }
+
+resource "aws_instance" "redis" {
+  ami           = local.ami_id
+  instance_type = "t3.micro"
+  subnet_id = local.database_subnet_id
+  vpc_security_group_ids = [local.redis_sg_id]
+  
+
+  tags = merge(
+    {
+        Name = "${var.project}-${var.environment}-redis"
+    },
+    local.common_tags
+  )
+}
+
+resource "terraform_data" "bootstrap_redis" {
+  triggers_replace =  [
+    aws_instance.redis.id
+  ]
+
+  connection {
+    type = "ssh"
+    user = "ec2-user"
+    password = "DevOps321"
+    host = aws_instance.redis.private_ip
+  }
+
+  provisioner "file" {
+    source      = "bootstrap.sh" # local file path
+    destination = "/tmp/bootstrap.sh" # destination file path on the remote mechine
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "chmod +x /tmp/bootstrap.sh",
+      "sudo sh /tmp/bootstrap.sh redis"
+    ]
+  }
+}
--- 40-databases/variables.tf
+++ 40-databases/variables.tf
@@ -6,3 +6,11 @@ variable "project" {
 variable "environment" {
     default = "dev"
 }
+
+variable "zone_id" {
+  default = "Z0700693PWL96G8VKYPV"
+}
+
+variable "domain_name" {
+  default = "soumyadevops.space"
+}
