9d0be11de851d49c93f35baac5fb964892e1e4a1
soumya <korlagani.soumya@gmail.com>
2026-03-22 15:22:25 +0530

    terraform

--- 30-bastion/main.tf
+++ 30-bastion/main.tf
@@ -3,7 +3,21 @@ resource "aws_instance" "bastion" {
   instance_type = "t3.micro"
   subnet_id = local.public_subnet_id
   vpc_security_group_ids = [local.bastion_sg_id]
-   iam_instance_profile = aws_iam_instance_profile.bastion.name
+  iam_instance_profile = aws_iam_instance_profile.bastion.name
+  user_data = file(bastion.sh)
+  root_block_device {
+    volume_size = 50
+    volume_type = "gp3"
+    # EBS volume tags
+    tags = merge(
+      {
+        Name = "${var.project}-${var.environment}-bastion"
+      },
+      local.common_tags
+    )
+  }
+  
+  
   tags = merge(
     {
         Name = "${var.project}-${var.environment}-bastion"
