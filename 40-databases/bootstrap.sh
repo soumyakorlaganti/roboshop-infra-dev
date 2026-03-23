cb13a875347afb95bdf604e00f18ac50a379d96d
soumya <korlagani.soumya@gmail.com>
2026-03-23 14:47:43 +0530

    terraform

--- 40-databases/bootstrap.sh
+++ 40-databases/bootstrap.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 component=$1
+environment=$2
 dnf install ansible -y
 
 cd /home/ec2-user
@@ -8,4 +9,4 @@ git clone https://github.com/soumyakorlaganti/ansible-roboshop-roles-tf.git
 
 cd ansible-roboshop-roles-tf
 git pull
-ansible-playbook -e component=$component roboshop.yaml
+ansible-playbook -e component=$component -e environment=$environment roboshop.yaml
--- 40-databases/main.tf
+++ 40-databases/main.tf
@@ -113,7 +113,7 @@ resource "terraform_data" "mysql" {
   provisioner "remote-exec" {
     inline = [
       "chmod +x /tmp/bootstrap.sh",
-      "sudo sh /tmp/bootstrap.sh mysql"
+      "sudo sh /tmp/bootstrap.sh mysql ${var.environment}"
     ]
   }
 }
