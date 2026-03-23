1afb15abfd41a0cd58e79b449beecebd91913e5d
soumya <korlagani.soumya@gmail.com>
2026-03-23 15:11:11 +0530

    terraform

--- 20-sg-rules/data.tf
+++ 20-sg-rules/data.tf
@@ -22,6 +22,10 @@ data "aws_ssm_parameter" "mysql_sg_id" {
   name = "/${var.project}/${var.environment}/mysql_sg_id"
 }
 
+data "aws_ssm_parameter" "rabbitmq_sg_id" {
+  name = "/${var.project}/${var.environment}/rabbitmq_sg_id"
+}
+
 data "aws_ssm_parameter" "catalogue_sg_id" {
   name = "/${var.project}/${var.environment}/catalogue_sg_id"
 }
